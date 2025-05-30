bac9d72ada00b68da98a3a14043560a5316dddd5
Guilherme Miranda <saguilherme16@gmail.com>
2025-05-30 19:19:06 -0300

    FEAT: add abstract class and logic

--- src/inheritance2/entities/TaxPayer.java
+++ src/inheritance2/entities/TaxPayer.java
@@ -1,4 +1,32 @@
 package inheritance2.entities;
 
-public class TaxPayer {
+public abstract class TaxPayer {
+    private String name;
+    private Double annualIncome;
+
+    public TaxPayer() {
+    }
+
+    public TaxPayer(String name, Double annualIncome) {
+        this.name = name;
+        this.annualIncome = annualIncome;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getAnnualIncome() {
+        return annualIncome;
+    }
+
+    public void setAnnualIncome(Double annualIncome) {
+        this.annualIncome = annualIncome;
+    }
+
+    public abstract Double tax();
 }
