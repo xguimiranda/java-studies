555f750648c08431fefdfe245172ad1319dca8c1
Guilherme Miranda <saguilherme16@gmail.com>
2025-05-30 18:46:14 -0300

    FEAT: add logic chose color

--- src/inheritance/program/Program.java
+++ src/inheritance/program/Program.java
@@ -8,6 +8,19 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter the number of shapes: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <=n; i++){
+            System.out.println("Shape #"+ i + " data:");
+            System.out.print("Rectangle or Circle (r/c)? ");
+            char question = sc.next().toLowerCase().charAt(0);
+            System.out.print("Color (BLACK/BLUE/RED): ");
+            String color = sc.next();
+            while (!color.equals("BLACK") && !color.equals("BLUE") && !color.equals("RED")){
+                System.out.print("Only type: (BLACK/BLUE/RED): ");
+                color = sc.next();
+            }
+        }
 
 
         
