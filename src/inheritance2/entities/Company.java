5dfbc1f81abd52cb550239d8715dcdc6c0969ec0
Guilherme Miranda <saguilherme16@gmail.com>
2025-05-30 19:29:44 -0300

    FEAT: add Company class and logic

--- src/inheritance2/entities/Company.java
+++ src/inheritance2/entities/Company.java
@@ -1,4 +1,32 @@
 package inheritance2.entities;
 
-public class Company {
+public class Company extends TaxPayer{
+    private int employeesNumber;
+
+    public Company() {
+    }
+
+    public Company(String name, Double annualIncome, int employeesNumber) {
+        super(name, annualIncome);
+        this.employeesNumber = employeesNumber;
+    }
+
+    public int getEmployeesNumber() {
+        return employeesNumber;
+    }
+
+    public void setEmployeesNumber(int employeesNumber) {
+        this.employeesNumber = employeesNumber;
+    }
+
+    @Override
+    public Double tax(){
+        double tax;
+        if (employeesNumber < 10){
+            tax = 0.16;
+        }else {
+            tax = 0.14;
+        }
+        return getAnnualIncome() * tax;
+    }
 }
