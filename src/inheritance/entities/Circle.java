d6f57edce24e904393200e3e2a585fb0a9b583a5
Guilherme Miranda <saguilherme16@gmail.com>
2025-05-30 18:39:19 -0300

    FEAT: add class circle

--- src/inheritance/entities/Circle.java
+++ src/inheritance/entities/Circle.java
@@ -1,4 +1,28 @@
 package inheritance.entities;
 
-public class Circle {
+import inheritance.entities.enums.Color;
+
+public class Circle extends Shape {
+    private Double radius;
+
+    public Circle() {
+    }
+
+    public Circle(Color color, Double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public Double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(Double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double area(){
+        return radius*2;
+    }
 }
