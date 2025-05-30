02202f001cfa40123044cc73a4efbc87ea8f0508
Guilherme Miranda <saguilherme16@gmail.com>
2025-05-30 19:25:34 -0300

    FEAT: add Individual class and logic

--- src/inheritance2/entities/Individual.java
+++ src/inheritance2/entities/Individual.java
@@ -1,4 +1,38 @@
 package inheritance2.entities;
 
-public class Individual {
+public class Individual extends  TaxPayer{
+    private Double healthcareCosts;
+
+    public Individual() {
+    }
+
+    public Individual(String name, Double annualIncome, Double healthcareCosts) {
+        super(name, annualIncome);
+        this.healthcareCosts = healthcareCosts;
+    }
+
+    public Double getHealthcareCosts() {
+        return healthcareCosts;
+    }
+
+    public void setHealthcareCosts(Double healthcareCosts) {
+        this.healthcareCosts = healthcareCosts;
+    }
+
+    @Override
+    public Double tax(){
+        double tax;
+        double discount;
+        if (getAnnualIncome() <= 20000){
+            tax = 0.15;
+        }else {
+            tax = 0.25;
+        }
+        if (getHealthcareCosts() < 0){
+            discount = getHealthcareCosts() * 0.50;
+            return getAnnualIncome() * tax - getHealthcareCosts() * discount;
+        }else {
+            return getHealthcareCosts() * tax;
+        }
+    }
 }
