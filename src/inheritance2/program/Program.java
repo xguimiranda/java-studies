141f041bdd251acafe47541bea4eb9b670fbe3be
Guilherme Miranda <saguilherme16@gmail.com>
2025-05-30 19:41:22 -0300

    feat : add the last logic

--- src/inheritance2/program/Program.java
+++ src/inheritance2/program/Program.java
@@ -1,5 +1,14 @@
 package inheritance2.program;
 
+import inheritance2.entities.Company;
+import inheritance2.entities.Individual;
+import inheritance2.entities.TaxPayer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
@@ -7,6 +16,7 @@ public class Program {
 
         List<TaxPayer> list = new ArrayList<>();
 
+        double totalTaxes = 0;
         System.out.print("Enter the number of tax payers: ");
         int n = sc.nextInt();
         for (int i = 1; i <= n; i++){
@@ -28,6 +38,14 @@ public class Program {
                 list.add(new Company(name, anualIncome, employeesNumber));
             }
         }
+        System.out.println();
+        System.out.println("TAXES PAID:");
+        for (TaxPayer taxPayer : list){
+            System.out.println(taxPayer.getName() + ": $ "+ String.format("%.2f", taxPayer.tax()));
+            totalTaxes += taxPayer.tax();
+        }
+        System.out.println();
+        System.out.println("TOTAL TAXES: $ "+ String.format("%.2f", totalTaxes));
 
         sc.close();
     }
