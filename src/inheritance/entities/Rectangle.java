211eda692b031cd77a0e9b94057bd320b22b0475
Guilherme Miranda <saguilherme16@gmail.com>
2025-05-30 18:34:09 -0300

    FEAT: add rectangle class

--- src/inheritance/entities/Rectangle.java
+++ src/inheritance/entities/Rectangle.java
@@ -1,4 +1,35 @@
 package inheritance.entities;
 
-public class Rectangle {
+public class Rectangle extends Shape {
+    private Double width;
+    private Double height;
+
+    public Rectangle() {
+    }
+
+    public Rectangle(Double width, Double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Double getWidth() {
+        return width;
+    }
+
+    public void setWidth(Double width) {
+        this.width = width;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    @Override
+    public double area() {
+        return width*height;
+    }
 }
