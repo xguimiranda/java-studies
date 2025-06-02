8764b7346384534f0e90e4419ab9fd276ff92fd2
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-02 17:41:10 -0300

    FEAT: create program class initial logic

--- src/exceptions/program/Program.java
+++ src/exceptions/program/Program.java
@@ -1,5 +1,7 @@
 package exceptions.program;
 
+import exceptions.model.entities.Account;
+
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -7,6 +9,23 @@ public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter your account data: ");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        System.out.print("Holder: ");
+        String holder = sc.next();
+        System.out.print("Initial balance: ");
+        double balance = sc.nextDouble();
+        System.out.print("WithDraw limit: ");
+        double withDrawLimit = sc.nextDouble();
+        Account acc = new Account(number, holder, balance, withDrawLimit);
+
+        System.out.println();
+        System.out.print("Enter amount for withdraw: ");
+        double amount = sc.nextDouble();
+        acc.withDraw(amount);
+
 
         sc.close();
     }
