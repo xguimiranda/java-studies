27748bed875a425f39a627a4f369bfd7b92aaf1d
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-02 20:23:52 -0300

    REFACTOR: refactor the code

--- src/exceptions/program/Program.java
+++ src/exceptions/program/Program.java
@@ -12,22 +12,24 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        try {
-            System.out.println("Enter your account data: ");
-            System.out.print("Number: ");
-            int number = sc.nextInt();
-            System.out.print("Holder: ");
-            String holder = sc.next();
-            System.out.print("Initial balance: ");
-            sc.nextLine();
-            double balance = sc.nextDouble();
-            System.out.print("WithDraw limit: ");
-            double withDrawLimit = sc.nextDouble();
-            Account acc = new Account(number, holder, balance, withDrawLimit);
+        System.out.println("Enter your account data: ");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+        System.out.print("Holder: ");
+        String holder = sc.next();
+        System.out.print("Initial balance: ");
+        sc.nextLine();
+        double balance = sc.nextDouble();
+        System.out.print("WithDraw limit: ");
+        double withDrawLimit = sc.nextDouble();
+        Account acc = new Account(number, holder, balance, withDrawLimit);
 
-            System.out.println();
-            System.out.print("Enter amount for withdraw: ");
-            double amount = sc.nextDouble();
+        System.out.println();
+        System.out.print("Enter amount for withdraw: ");
+        double amount = sc.nextDouble();
+
+        
+        try {
             acc.withDraw(amount);
             System.out.println(String.format("New balance: %.2f"+ acc.getBalance()));
         }
