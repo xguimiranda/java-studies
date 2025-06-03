24795d8d346f12c21aa02c13956e7f95fc9f5645
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-02 21:57:59 -0300

    FEAT: Initial program logic

--- src/movieRental/program/Program.java
+++ src/movieRental/program/Program.java
@@ -1,5 +1,16 @@
 package movieRental.program;
 
-public class Program {
 
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+
+
+        sc.close();
+    }
 }
