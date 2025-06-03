6158d48ce484994bbaf41476dde1cb008e414581
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-02 21:48:17 -0300

    FEAT: create rent class

--- src/movieRental/model/entities/Rent.java
+++ src/movieRental/model/entities/Rent.java
@@ -1,4 +1,33 @@
 package movieRental.model.entities;
 
 public class Rent {
+    private String filmName;
+    private LocalDate rentDate;
+    private LocalDate instanteDate;
+
+    public Rent(String filmName, LocalDate rentDate, LocalDate instanteDate) {
+        this.filmName = filmName;
+        this.rentDate = rentDate;
+        this.instanteDate = instanteDate;
+    }
+
+    public String getFilmName() {
+        return filmName;
+    }
+
+    public void setFilmName(String filmName) {
+        this.filmName = filmName;
+    }
+
+    public LocalDate getRentDate() {
+        return rentDate;
+    }
+
+    public LocalDate getInstanteDate() {
+        return instanteDate;
+    }
+
+    public long duration(){
+        return ChronoUnit.DAYS.between(instanteDate, rentDate);
+    }
 }
