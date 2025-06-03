a3207821b13cc52ec68444942cf227b48a6f8ccc
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-02 21:56:02 -0300

    FEAT: create film class

--- src/movieRental/model/entities/Film.java
+++ src/movieRental/model/entities/Film.java
@@ -1,4 +1,41 @@
 package movieRental.model.entities;
 
 public class Film {
+    private String filmName;
+    private Gender gender;
+    private Double price;
+
+    public Film() {
+    }
+
+    public Film(String filmName, Gender gender, Double price) {
+        this.filmName = filmName;
+        this.gender = gender;
+        this.price = price;
+    }
+
+    public String getFilmName() {
+        return filmName;
+    }
+
+    public void setFilmName(String filmName) {
+        this.filmName = filmName;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
 }
