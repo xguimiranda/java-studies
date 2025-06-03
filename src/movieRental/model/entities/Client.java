371228b81c9939c5202169c1f85226b1e4fd51ae
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-02 21:32:11 -0300

    FEAT: client class

--- src/movieRental/model/entities/Client.java
+++ src/movieRental/model/entities/Client.java
@@ -1,4 +1,28 @@
 package movieRental.model.entities;
 
 public class Client {
+    private String name;
+    private Integer cpf;
+
+    public Client() {
+    }
+
+    public Client(String name, Integer cpf) {
+        this.name = name;
+        this.cpf = cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getCpf() {
+        return cpf;
+    }
+
+    
 }
