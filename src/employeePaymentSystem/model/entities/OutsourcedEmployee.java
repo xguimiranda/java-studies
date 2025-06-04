18536977c465e24aa817f7fdc72bae1ad19f60ee
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-03 22:20:28 -0300

    FEAT: create override/extends class

--- src/employeePaymentSystem/model/entities/OutsourcedEmployee.java
+++ src/employeePaymentSystem/model/entities/OutsourcedEmployee.java
@@ -1,4 +1,24 @@
 package employeePaymentSystem.model.entities;
 
-public class OutsourcedEmployee {
+public class OutsourcedEmployee extends Employee{
+
+    private Double additionalCharge;
+
+    public OutsourcedEmployee(String name, Double hourlyRate, Integer hours, Double additionalCharge) {
+        super(name, hourlyRate, hours);
+        this.additionalCharge = additionalCharge;
+    }
+
+    public Double getAdditionalCharge() {
+        return additionalCharge;
+    }
+
+    public void setAdditionalCharge(Double additionalCharge) {
+        this.additionalCharge = additionalCharge;
+    }
+
+    @Override
+    public final double payment(){
+        return super.payment() + (additionalCharge * 1.1);
+    }
 }
