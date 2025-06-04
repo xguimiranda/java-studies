6fad20dd5b34c3bc5beefd281652c99ad61ddc62
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-03 22:30:03 -0300

    FEAT: add Program initial logic

--- src/employeePaymentSystem/application/Program.java
+++ src/employeePaymentSystem/application/Program.java
@@ -1,4 +1,45 @@
 package employeePaymentSystem.application;
 
 public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        List<Employee> list = new ArrayList<>();
+
+        System.out.print("Enter the number of employees: ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++){
+            System.out.println("Employee #" + i + " data:");
+            System.out.print("Outsourced (y/n)? ");
+            char question = sc.next().toLowerCase().charAt(0);
+            while (question != 'y' && question != 'n'){
+                System.out.print("Outsourced, only use yes our no(y/n)? ");
+                question = sc.next().toLowerCase().charAt(0);
+            }
+            System.out.print("name: ");
+            String name = sc.next();
+            System.out.print("Hours: ");
+            sc.nextLine();
+            int hours = sc.nextInt();
+            System.out.print("Hourly Rate: ");
+            double hourlyRate = sc.nextDouble();
+            if (question == 'y'){
+                System.out.println("Additional charge: ");
+                double additionalCharge = sc.nextDouble();
+                list.add(new OutsourcedEmployee(name, hourlyRate, hours, additionalCharge));
+            }else{
+                list.add(new RegularEmployee(name, hourlyRate, hours));
+            }
+        }
+        System.out.println();
+        System.out.println("PAYMENTS:");
+        for (Employee employee : list){
+            System.out.println(employee);
+        }
+
+
+        sc.close();
+    }
 }
