14777c5e57e230fa26b272060bbb97a5a2d87459
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-03 22:12:15 -0300

    FEAT: create initial classes

---
+++ src/employeePaymentSystem/application/Program.java
@@ -0,0 +1,4 @@
+package employeePaymentSystem.application;
+
+public class Program {
+}
---
+++ src/employeePaymentSystem/model/entities/employee.java
@@ -0,0 +1,41 @@
+package employeePaymentSystem.model.entities;
+
+public abstract class employee {
+    private String name;
+    private Double hourlyRate;
+    private Integer hours;
+
+    public employee(String name, Double hourlyRate, Integer hours) {
+        this.name = name;
+        this.hourlyRate = hourlyRate;
+        this.hours = hours;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getHourlyRate() {
+        return hourlyRate;
+    }
+
+    public void setHourlyRate(Double hourlyRate) {
+        this.hourlyRate = hourlyRate;
+    }
+
+    public Integer getHours() {
+        return hours;
+    }
+
+    public void setHours(Integer hours) {
+        this.hours = hours;
+    }
+
+    public double payment(){
+        return hourlyRate* hours;
+    }
+}
