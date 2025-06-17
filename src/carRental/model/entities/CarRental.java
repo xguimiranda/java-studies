0142d1b38c0452fc017ca37f1be03de71e484ee6
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-17 20:27:13 -0300

    FEAT: crate CarRental class

---
+++ src/carRental/model/entities/CarRental.java
@@ -0,0 +1,50 @@
+package carRental.model.entities;
+
+import java.time.LocalDate;
+
+public class CarRental {
+
+    private LocalDate start;
+    private LocalDate finish;
+
+    private Vehicle vehicle;
+    private Invoice invoice;
+
+    public CarRental(LocalDate start, LocalDate finish, Vehicle vehicle) {
+        this.start = start;
+        this.finish = finish;
+        this.vehicle = vehicle;
+    }
+
+    public LocalDate getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDate start) {
+        this.start = start;
+    }
+
+    public LocalDate getFinish() {
+        return finish;
+    }
+
+    public void setFinish(LocalDate finish) {
+        this.finish = finish;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public Invoice getInvoice() {
+        return invoice;
+    }
+
+    public void setInvoice(Invoice invoice) {
+        this.invoice = invoice;
+    }
+}
