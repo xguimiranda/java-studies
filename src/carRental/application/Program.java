c730b25005751727c10fd2e43a8fb32e4b2e6037
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-17 20:38:23 -0300

    FEAT: initial program logic

--- src/carRental/application/Program.java
+++ src/carRental/application/Program.java
@@ -1,4 +1,39 @@
 package carRental.application;
 
+import carRental.model.entities.CarRental;
+import carRental.model.entities.Vehicle;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entre com os dados do aluguel");
+        System.out.print("Modelo do carro: ");
+        String modelo = sc.nextLine();
+        System.out.print("Retirada (dd/MM/yyyy hh:mm): ");
+        LocalDate start = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+        System.out.print("Retorno (dd/MM/yyyy hh:mm): ");
+        LocalDate finish = LocalDate.parse(sc.next(), DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm"));
+        CarRental cr = new CarRental(start, finish, new Vehicle(modelo));
+
+        System.out.print("Entre com o preço por hora: ");
+        double pricePerHour = sc.nextDouble();
+        System.out.print("Entre com o preço por dia: ");
+        double pricePerDay = sc.nextDouble();
+
+
+        System.out.println("FATURA:");
+        System.out.println("Pagamento basico: ");
+        System.out.println("Imposto: ");
+        System.out.println("Pagamento total: ");
+
+        sc.close();
+    }
 }
