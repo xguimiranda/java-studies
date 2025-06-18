a6ab20617edb49e8f5addea184db05d0b563731b
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-18 16:27:03 -0300

    FEAT: add processContract logic

--- src/interfaceExercise2/model/service/ContractService.java
+++ src/interfaceExercise2/model/service/ContractService.java
@@ -12,5 +12,15 @@ public class ContractService {
 
     public void processContract(Contract contract, int mounts){
 
+        double basicQuota = contract.getTotalValue() / mounts;
+
+        for (int i = 1; i <= mounts; i++) {
+            LocalDate dueDate = contract.getDate().plusMonths(i);
+            double interest = onlinePaymentService.interest(basicQuota, i);
+            double fee = onlinePaymentService.paymentFee(basicQuota + interest);
+            double quota = basicQuota + fee + interest;
+
+            contract.getList().add(new Installment(dueDate, quota));
+        }
     }
 }
