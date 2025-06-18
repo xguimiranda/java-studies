4724ea0a1ad94b6ea2c30af08d9f2eb0ee1fa7b9
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-17 21:00:13 -0300

    FEAT: create BrazilTaxService class

---
+++ src/carRental/model/services/BrazilTaxService.java
@@ -0,0 +1,14 @@
+package carRental.model.services;
+
+public class BrazilTaxService {
+
+    public double tax (double amount){
+    if (amount <= 100.00){
+        return 0.20;
+    }
+    else {
+        return 0.10;
+    }
+    }
+
+}
