116eaf158a3fc1d820a207f07a05edeb7eafc0d9
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-17 21:11:34 -0300

    FEAT: add the constructor

--- src/carRental/model/entities/Invoice.java
+++ src/carRental/model/entities/Invoice.java
@@ -5,6 +5,10 @@ public class Invoice {
     private double basicPayment;
     private double tax;
 
+    public Invoice(double basicPayment, double tax) {
+        this.basicPayment = basicPayment;
+        this.tax = tax;
+    }
 
     public double totalPayment(){
        return basicPayment + tax;
