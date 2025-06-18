cc5fdbd02767f446e7ecdf409b2c5847fc0d4667
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-17 21:00:28 -0300

    FEAT: create RentalService class

---
+++ src/carRental/model/services/RentalService.java
@@ -0,0 +1,15 @@
+package carRental.model.services;
+
+import carRental.model.entities.CarRental;
+
+public class RentalService {
+
+    private double pricePerHour;
+    private double pricePerDay;
+
+    private BrazilTaxService brazilTaxService;
+
+    public void processInvoice(CarRental carRental){
+
+    }
+}
