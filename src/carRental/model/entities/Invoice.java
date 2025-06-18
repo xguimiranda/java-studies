206cbf245fc66265ca6393966aac6c560450797d
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-17 21:39:35 -0300

    FEAT: add gets and setters

--- src/carRental/model/entities/Invoice.java
+++ src/carRental/model/entities/Invoice.java
@@ -13,4 +13,20 @@ public class Invoice {
     public double totalPayment(){
        return basicPayment + tax;
     }
+
+    public double getBasicPayment() {
+        return basicPayment;
+    }
+
+    public void setBasicPayment(double basicPayment) {
+        this.basicPayment = basicPayment;
+    }
+
+    public double getTax() {
+        return tax;
+    }
+
+    public void setTax(double tax) {
+        this.tax = tax;
+    }
 }
