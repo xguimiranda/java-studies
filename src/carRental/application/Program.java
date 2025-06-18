2ed8bcc724ad017c7cafe6df31c3c8e28f05b68d
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-17 22:00:26 -0300

    REFACTOR: clear the useless imports

--- src/carRental/application/Program.java
+++ src/carRental/application/Program.java
@@ -5,8 +5,7 @@ import carRental.model.entities.Vehicle;
 import carRental.model.services.BrazilTaxService;
 import carRental.model.services.RentalService;
 
-
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
