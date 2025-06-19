a9f16c3f885698823525c788da56a3a930a0f895
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-19 15:03:07 -0300

    FEAT: add imports

--- src/votacaoContagemMap/application/Program.java
+++ src/votacaoContagemMap/application/Program.java
@@ -1,5 +1,15 @@
 package votacaoContagemMap.application;
 
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Program {
 
 }
