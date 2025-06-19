11e2cbe2f8845de503d2579e1a48fd411aca0913
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-19 14:30:52 -0300

    FEAT: program logic

--- src/exerciseSet1/application/Program.java
+++ src/exerciseSet1/application/Program.java
@@ -8,4 +8,30 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Set<StudentAccountant> set = new HashSet<>();
+
+        System.out.print("How many students for course A? ");
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++){
+            set.add(new StudentAccountant(sc.nextInt()));
+        }
+        System.out.print("How many students for course B? ");
+        n = sc.nextInt();
+        for (int i = 1; i <= n; i++){
+            set.add(new StudentAccountant(sc.nextInt()));
+        }
+        System.out.print("How many students for course C? ");
+        n = sc.nextInt();
+        for (int i = 1; i <= n; i++){
+            set.add(new StudentAccountant(sc.nextInt()));
+        }
+
+        System.out.println("Total students: "+ set.size());
+
+        sc.close();
+    }
 }
