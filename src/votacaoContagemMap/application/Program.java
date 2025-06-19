4e6e3c493533e4674df39a049ba95dcd07be940b
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-19 15:03:23 -0300

    FEAT: initial program logic

--- src/votacaoContagemMap/application/Program.java
+++ src/votacaoContagemMap/application/Program.java
@@ -11,5 +11,17 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Map<String, Integer> votes = new LinkedHashMap<>();
+
+        System.out.println("Enter file full path: ");
+        String path = sc.next();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+
+            String line = br.readLine();
 
 }
