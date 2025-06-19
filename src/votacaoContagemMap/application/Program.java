0bb76902cff3452df89cbf226dc59956927aca52
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-19 15:03:50 -0300

    REFACTOR: imports

--- src/votacaoContagemMap/application/Program.java
+++ src/votacaoContagemMap/application/Program.java
@@ -2,7 +2,6 @@ package votacaoContagemMap.application;
 
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.LinkedHashMap;
