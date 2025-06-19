c0b13994ceab726ed8d35feb175f720f1590d740
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-19 14:30:42 -0300

    FEAT: add all the imports

--- src/exerciseSet1/application/Program.java
+++ src/exerciseSet1/application/Program.java
@@ -1,4 +1,11 @@
 package exerciseSet1.application;
 
+import exerciseSet1.model.entities.StudentAccountant;
+
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Set;
+
 public class Program {
 }
