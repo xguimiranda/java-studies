f46c0f8f281a47671d313b4f20d65c9592a96499
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-20 18:51:17 -0300

    FEAT: create Product class

---
+++ src/exerciseLambda1/Program.java
@@ -0,0 +1,23 @@
+package exerciseLambda1;
+
+import exerciseLambda1.entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("TV", 900.00));
+        list.add(new Product("Notebook", 1200.00));
+        list.add(new Product("Tablet", 450.00));
+
+
+
+
+    }
+}
