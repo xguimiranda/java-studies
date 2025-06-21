657cf98984e05cbf9a347bc2f2366c842ecad61a
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-20 21:19:14 -0300

    REFACTOR: finished code

--- src/exerciseLambda2/application/Program.java
+++ src/exerciseLambda2/application/Program.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Program {
     public static void main(String[] args) {
