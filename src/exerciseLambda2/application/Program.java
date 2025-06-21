63b153bcc0d6321c48b45b8060bb1ebc92bc5ee2
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-20 21:06:31 -0300

    FIX: imports

--- src/exerciseLambda2/application/Program.java
+++ src/exerciseLambda2/application/Program.java
@@ -1,5 +1,12 @@
 package exerciseLambda2.application;
 
+import exerciseLambda2.entities.Product;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 public class Program {
