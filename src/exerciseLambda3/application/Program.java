136a7b2624896d6d479fd4c7a6dc962d55835929
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-20 21:30:25 -0300

    FEAT: initial Imports

--- src/exerciseLambda3/application/Program.java
+++ src/exerciseLambda3/application/Program.java
@@ -1,4 +1,15 @@
 package exerciseLambda3.application;
 
+
+import exerciseLambda3.entities.Employee;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 }
