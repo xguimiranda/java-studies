580aea4bb196d5a17f242b85f2e6b7ac00caccc7
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-20 21:07:15 -0300

    FEAT: create Program initial logic

--- src/exerciseLambda2/application/Program.java
+++ src/exerciseLambda2/application/Program.java
@@ -8,11 +8,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+
+        System.out.print("Enter full file path: ");
+        String path = sc.next();
 
         try(BufferedReader br = new BufferedReader(new FileReader(path))){
 
