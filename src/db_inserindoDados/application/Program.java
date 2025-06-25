a2a4d1fdb862181f9fb90ffff798afc8f73bfd8a
Guilherme Miranda <saguilherme16@gmail.com>
2025-06-25 18:24:30 -0300

    FEAT: Implement insert functionality for seller in Program class

--- src/db_inserindoDados/application/Program.java
+++ src/db_inserindoDados/application/Program.java
@@ -1,11 +1,48 @@
 package db_inserindoDados.application;
 
+import db_inserindoDados.DB;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Program {
     public static void main(String[] args) {
 
+    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+    Connection conn = null;
+    PreparedStatement st = null;
+        try{
+            conn = DB.getConnection();
 
+            st = conn.prepareStatement(
+                    "INSERT INTO seller " +
+                    "(Name, Email, Birthdate, BaseSalary, DepartmentID) " +
+                    "VALUES " +
+                    "(?, ?, ?, ?, ?)");
+
+            st.setString(1, "Carl Purple");
+            st.setString(2, "carl@gamil.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("24/06/1998").getTime()));
+            st.setDouble(4, 3000.0);
+            st.setInt(5, 4);
+
+            int rowsAffected = st.executeUpdate();
+
+            System.out.println("Done! Rows affected: " + rowsAffected);
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+        }
+        catch (ParseException e){
+            e.printStackTrace();
+        }
+        finally{
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
     }
 }
 
